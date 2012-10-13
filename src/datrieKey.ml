9b881da1f984c9bac152adc72e950c9cf40cbb6b
Yutaka ICHIBANGASE <yichiban@gmail.com>
2012-10-13 15:10:51 +0900

    added utf8 key and rope key

--- src/datrieKey.ml
+++ src/datrieKey.ml
@@ -16,3 +16,37 @@ struct
 
   let input = Char.chr
 end
+
+module UTF8Key =
+struct
+  type t = BatUTF8.t
+
+  type input = Char.t
+
+  let enum = BatUTF8.to_string |- BatString.enum
+
+  let of_enum = BatString.of_enum |- BatUTF8.adopt
+
+  let all = BatChar.enum |- BatSet.of_enum
+
+  let code = Char.code
+
+  let input = Char.chr
+end
+
+module RopeKey =
+struct
+  type t = BatRope.t
+
+  type input = Char.t
+
+  let enum = BatRope.to_string |- BatString.enum
+
+  let of_enum = BatString.of_enum |- BatRope.of_string
+
+  let all = BatChar.enum |- BatSet.of_enum
+
+  let code = Char.code
+
+  let input = Char.chr
+end
--- src/datrieKey.mli
+++ src/datrieKey.mli
@@ -1,1 +1,3 @@
 module StringKey : DatrieInterface.Key with type t = String.t
+module UTF8Key : DatrieInterface.Key with type t = BatUTF8.t
+module RopeKey : DatrieInterface.Key with type t = BatRope.t
